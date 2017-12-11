8994bb5e494fcc9e812b6a82023faa476ab47f04
Ubuntu <farasatha@wso2.com>
2017-12-11 10:23:45 +0000

    Copy GC logs from identity server

--- jmeter_run_all.sh
+++ jmeter_run_all.sh
@@ -35,7 +35,7 @@ carbon_home="/home/ubuntu/is_540/wso2is-5.4.0-beta"
 java_home="/home/ubuntu/software/jdk1.8.0_144"
 jmeter_home="/home/ubuntu/software/apache-jmeter-3.3"
 output_directory="/home/ubuntu/output"
-private_key_path="is540pf.pem"
+private_key_path="/home/ubuntu/is540pf.pem"
 
 identity_db_host="db2.wso2.com"
 identity_db_username="root"
@@ -58,6 +58,10 @@ do
 	#carbon_home="/home/ubuntu/is_540/wso2is-5.4.0-beta"
 	echo $carbon_home
 	ssh -i $private_key_path $is_540_host_username@$is_540_host << ENDSSH
+	# cleanup any prev session files
+	rm -f $carbon_home/repository/logs/gc_log_$scenario$concurrency.log
+	rm -f ~/sar_$scenario$concurrency.log
+
         echo "Killing All Carbon Servers......"
         killall java
 		export JAVA_HOME=$java_home
@@ -79,12 +83,19 @@ ENDSSH
 	sudo mysqlslap --user=$identity_db_username --password=$identity_db_password --host=$identity_db_host --concurrency=50 --iterations=10 --auto-generate-sql --verbose >> $output_directory/mySQLSlap_$concurrency.log
 	ssh -i $private_key_path $is_540_host_username@$is_540_host << ENDSSH2
 	echo "************************"
-	cp $carbon_home/repository/logs/gc.log gc_log_$scenario$concurrency.log
-	sar -q > $(date +`hostname`-%d-%m-%y-%H%M_log_$scenario$concurrency.log)
+	mv $carbon_home/repository/logs/gc.log $carbon_home/repository/logs/gc_log_$scenario$concurrency.log
+	
+	sar -q > sar_$scenario$concurrency.log
         echo Killing All Carbon Servers......
         killall java
 	exit
 ENDSSH2
+	echo "Copying GC logs from Identity Server...."
+	scp -i $private_key_path $is_540_host_username@$is_540_host:$carbon_home/repository/logs/gc_log_$scenario$concurrency.log $output_directory/gc_log_$concurrency.log
+	echo "Copying SAR logs from Identity Server...."
+	scp -i $private_key_path $is_540_host_username@$is_540_host:~/sar_$scenario$concurrency.log $output_directory/sar_log_$concurrency.log
+
+
 	echo "=========================== CONCURRENCY LEVEL: $concurrency ended ============================"
 done
 echo "************************ End Performance test for $scenario using script: $script_location ***************************************"

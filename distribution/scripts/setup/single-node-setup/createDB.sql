20c33be38bb8c416b7a353ea237d0c1430d7b24c
Inthirakumaaran <inthira.14@cse.mrt.ac.lk>
2019-03-01 12:21:04 +0530

    moving table creations

--- distribution/scripts/setup/single-node-setup/createDB.sql
+++ distribution/scripts/setup/single-node-setup/createDB.sql
@@ -5,3 +5,7 @@ create database REG_DB;
 use IDENTITY_DB; source ~/wso2is/dbscripts/identity/mysql-5.7.sql;
 use UM_DB; source ~/wso2is/dbscripts/mysql5.7.sql;
 use REG_DB; source ~/wso2is/dbscripts/mysql5.7.sql;
+
+-- add tables that could vary in different IS packs
+use IDENTITY_DB;
+CREATE TABLE IF NOT EXISTS IDN_AUTH_TEMP_SESSION_STORE(id int);

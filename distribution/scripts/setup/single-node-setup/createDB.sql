ef8a295efc913d0a2a7b7da2cde4976d83cd3632
achini <aaujayasena@gmail.com>
2019-09-11 18:48:49 +0530

    chnage dbscripts name  according to the wso2is5.9.0

--- distribution/scripts/setup/single-node-setup/createDB.sql
+++ distribution/scripts/setup/single-node-setup/createDB.sql
@@ -2,9 +2,9 @@ create database IDENTITY_DB;
 create database UM_DB;
 create database REG_DB;
 
-use IDENTITY_DB; source ~/wso2is/dbscripts/identity/mysql-5.7.sql;
-use UM_DB; source ~/wso2is/dbscripts/mysql5.7.sql;
-use REG_DB; source ~/wso2is/dbscripts/mysql5.7.sql;
+use IDENTITY_DB; source ~/wso2is/dbscripts/identity/mysql.sql;
+use UM_DB; source ~/wso2is/dbscripts/mysql.sql;
+use REG_DB; source ~/wso2is/dbscripts/mysql.sql;
 
 -- add tables that could vary in different IS packs
 use IDENTITY_DB;

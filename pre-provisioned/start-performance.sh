6eff73addb80a21282d17d8267b8dcca3f625f75
Buddhima Udaranga <buddhimau@wso2.com>
2021-01-14 23:07:14 +0530

    Add readme

--- pre-provisioned/start-performance.sh
+++ pre-provisioned/start-performance.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-# Copyright (c) 2019, wso2 Inc. (http://wso2.org) All Rights Reserved.
+# Copyright (c) 2021, wso2 Inc. (http://wso2.org) All Rights Reserved.
 #
 # wso2 Inc. licenses this file to you under the Apache License,
 # Version 2.0 (the "License"); you may not use this file except

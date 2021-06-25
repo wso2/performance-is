c0a97884af80f6acf56858726a7c0b43ee3db774
senthalan <senthalank@gmail.com>
2021-06-25 11:12:33 +0530

    Add consent tables to Identity DB

--- single-node/setup/resources/createDB.sql
+++ single-node/setup/resources/createDB.sql
@@ -3,6 +3,7 @@ create database UM_DB;
 create database REG_DB;
 
 use IDENTITY_DB; source ~/wso2is/dbscripts/identity/mysql.sql;
+use IDENTITY_DB; source ~/wso2is/dbscripts/consent/mysql.sql;
 use UM_DB; source ~/wso2is/dbscripts/mysql.sql;
 use REG_DB; source ~/wso2is/dbscripts/mysql.sql;
 

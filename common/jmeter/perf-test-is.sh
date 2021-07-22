ce6537e0fe742b8fc5f98a0fd744cc6fe1564e38
dewniMW <dewni.matheesha@gmail.com>
2021-07-22 10:11:10 +0530

    Add TestData_Add_OAuth_Apps

--- common/jmeter/perf-test-is.sh
+++ common/jmeter/perf-test-is.sh
@@ -343,7 +343,7 @@ function run_test_data_scripts() {
 
     echo "Running test data setup scripts"
     echo "=========================================================================================="
-    declare -a scripts=("TestData_SCIM2_Add_User.jmx" )
+    declare -a scripts=("TestData_SCIM2_Add_User.jmx" "TestData_Add_OAuth_Apps.jmx")
 #    declare -a scripts=("TestData_Add_Super_Tenant_Users.jmx" "TestData_Add_OAuth_Apps.jmx" "TestData_Add_SAML_Apps.jmx" "TestData_Add_Tenants.jmx" "TestData_Add_Tenant_Users.jmx")
     setup_dir="/home/ubuntu/workspace/jmeter/setup"
 

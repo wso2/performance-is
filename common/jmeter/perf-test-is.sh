a76774dc843f4f2586556b021c7ec767de3555ec
Sachin-Mamoru <sachinm@wso2.com>
2023-06-07 01:08:51 +0530

    enable burst traffic through command line option

--- common/jmeter/perf-test-is.sh
+++ common/jmeter/perf-test-is.sh
@@ -92,6 +92,10 @@ mode=""
 jwt_token_client_secret=""
 jwt_token_user_password=""
 
+# Burst Traffic
+enable_burst=false
+burstTraffic=3000
+
 # Start time of the test
 test_start_time=$(date +%s)
 # Scenario specific counters
@@ -125,7 +129,7 @@ function usage() {
     echo ""
 }
 
-while getopts "c:m:d:w:j:i:e:n:s:q:u:t:p:k:v:o:h" opts; do
+while getopts "c:m:d:w:j:i:e:n:s:q:u:t:p:k:v:b:o:h" opts; do
     case $opts in
     c)
         concurrent_users+=("${OPTARG}")
@@ -171,6 +175,9 @@ while getopts "c:m:d:w:j:i:e:n:s:q:u:t:p:k:v:o:h" opts; do
         ;;
     v)
         mode=${OPTARG}
+        ;;
+    b)
+        enable_burst=${OPTARG}
         ;;
     k)
         jwt_token_client_secret=${OPTARG}
@@ -218,6 +225,13 @@ fi
 if ! [[ $jmeter_client_heap_size =~ $heap_regex ]]; then
     echo "Please specify a valid heap for JMeter Client."
     exit 1
+fi
+
+# Check if the variable is true
+if [ "$enable_burst" = true ]; then
+    burstTraffic=3000
+else
+    burstTraffic=0
 fi
 
 declare -ag heap_sizes_array
@@ -533,7 +547,7 @@ function test_scenarios() {
                 mkdir -p "$report_location"
 
                 time=$(expr "$test_duration" \* 60)
-                declare -ag jmeter_params=("concurrency=$users" "time=$time" "host=$lb_host" "-Jport=$is_port")
+                declare -ag jmeter_params=("concurrency=$users" "time=$time" "host=$lb_host" "-Jport=$is_port" "noOfBurst=$burstTraffic")
 
                 local tenantMode=${scenario[tenantMode]}
                 if [ "$tenantMode" = true ]; then

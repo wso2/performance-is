8ae3a5ace92634663ce4876117adf705135604e6
Sachin-Mamoru <sachinm@wso2.com>
2023-06-13 15:57:32 +0530

    parameterized the user tag for single node

--- single-node/start-performance.sh
+++ single-node/start-performance.sh
@@ -30,6 +30,7 @@ stack_name="is-performance-single-node--$timestamp--$random_number"
 
 # Cloud Formation parameters.
 
+user_tag=""
 key_file=""
 certificate_name=""
 jmeter_setup=""
@@ -74,8 +75,11 @@ function usage() {
     echo ""
 }
 
-while getopts "k:c:j:n:u:p:i:b:w:y:g:t:h" opts; do
+while getopts "q:k:c:j:n:u:p:i:b:w:y:g:t:h" opts; do
     case $opts in
+    q)
+        user_tag=${OPTARG}
+        ;;
     k)
         key_file=${OPTARG}
         ;;
@@ -127,6 +131,11 @@ shift "$((OPTIND - 1))"
 echo "Run mode: $mode"
 run_performance_tests_options="$@"
 run_performance_tests_options+=(" -v $mode -k $jwt_token_client_secret -o $jwt_token_user_password")
+
+if [[ -z $user_tag ]]; then
+    echo "Please provide the user tag."
+    exit 1
+fi
 
 if [[ ! -f $key_file ]]; then
     echo "Please provide the key file."
@@ -247,6 +256,7 @@ create_stack_command="aws cloudformation create-stack --stack-name $stack_name \
         ParameterKey=DBInstanceType,ParameterValue=$db_instance_type \
         ParameterKey=WSO2InstanceType,ParameterValue=$wso2_is_instance_type \
         ParameterKey=BastionInstanceType,ParameterValue=$bastion_instance_type \
+        ParameterKey=UserTag,ParameterValue=$user_tag \
     --capabilities CAPABILITY_IAM"
 
 echo ""

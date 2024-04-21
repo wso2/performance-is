0778914e88440a62d40f4195046fe7e4bd652673
Madhavi Gayathri <madhavi.17@cse.mrt.ac.lk>
2024-04-21 11:32:16 +0530

    Remove unrelated publish test scenario.

--- common/deployment/test_scenarios.sh
+++ common/deployment/test_scenarios.sh
@@ -78,7 +78,7 @@ declare -A test_scenario06=(
     [jmx]="oidc/OIDC_AuthCode_Redirect_WithoutConsent_Retrieve_User_Attributes.jmx"
     [tenantMode]=false
     [skip]=false
-    [modes]="FULL QUICK PUBLISH"
+    [modes]="FULL QUICK"
 )
 declare -A test_scenario07=(
     [name]="07-oidc_auth_code_redirect_without_consent_retrieve_user_attributes_and_groups"

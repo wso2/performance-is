21e813823bfb75538bcaa34207f099826100f384
Udara Pathum <46132469+hwupathum@users.noreply.github.com>
2024-11-20 16:19:50 +0530

    Add multiple db type scripts

--- common/deployment/setup/resources/mysql/createDB.sql
+++ common/deployment/setup/resources/mysql/createDB.sql
@@ -6,7 +6,3 @@ use IDENTITY_DB; source ~/wso2is/dbscripts/identity/mysql.sql;
 use IDENTITY_DB; source ~/wso2is/dbscripts/consent/mysql.sql;
 use UM_DB; source ~/wso2is/dbscripts/mysql.sql;
 use REG_DB; source ~/wso2is/dbscripts/mysql.sql;
-
--- add tables that could vary in different IS packs
-use IDENTITY_DB;
-CREATE TABLE IF NOT EXISTS IDN_AUTH_TEMP_SESSION_STORE(id int);

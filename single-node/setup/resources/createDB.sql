370e43106892fc20ce6dab9576db4f5479ff2edd
Udara Pathum <46132469+hwupathum@users.noreply.github.com>
2025-04-09 17:35:43 +0530

    Add character set specification for database creation in createDB.sql

--- single-node/setup/resources/createDB.sql
+++ single-node/setup/resources/createDB.sql
@@ -1,6 +1,6 @@
-create database IDENTITY_DB;
-create database UM_DB;
-create database REG_DB;
+create database IDENTITY_DB CHARACTER SET latin1;
+create database UM_DB CHARACTER SET latin1;
+create database REG_DB CHARACTER SET latin1;
 
 use IDENTITY_DB; source ~/wso2is/dbscripts/identity/mysql.sql;
 use IDENTITY_DB; source ~/wso2is/dbscripts/consent/mysql.sql;
